006d59d6ac5271c38afeee2f4c8bb0cd15e3f5c2
xiaoqinglin <xiaoqinglin2018@gmail.com>
2020-04-26 23:50:01 +0800

    [fix] add unit test case

--- store/serializers.py
+++ store/serializers.py
@@ -20,24 +20,38 @@ class ProductSerializer(serializers.ModelSerializer):
     price = serializers.DecimalField(min_value=1.00, max_value=100000,
                                      max_digits=None, decimal_places=2)
     sale_start = serializers.DateTimeField(
+        required=False,
         input_formats=['%Y-%m-%d %I:%M %p'], format=None, allow_null=True,
         help_text="Accept format is '2020-01-01 12:01 AM'",
         style={'input_type': 'text', 'placeholder': '2020-01-01 12:01 AM'}
     )
     sale_end = serializers.DateTimeField(
+        required=False,
         input_formats=['%Y-%m-%d %I:%M %p'], format=None, allow_null=True,
         help_text="Accept format is '2020-01-01 12:01 PM'",
         style={'input_type': 'text', 'placeholder': '2020-01-01 12:01 PM'}
     )
+    photo = serializers.ImageField(default=None)
+    warranty = serializers.FileField(write_only=True, default=None)
 
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'sale_start', 'sale_end',
-                  'is_on_sale', 'current_price', 'cart_items')
+                  'is_on_sale', 'current_price', 'cart_items', 'photo', 'warranty')
 
     def get_cart_items(self, instance):
         items = ShoppingCartItem.objects.filter(product=instance)
         return CartItemSerializer(items, many=True).data
+
+    def update(self, instance, validated_data):
+        if validated_data.get('warranty', None):
+            instance.description += '\n\n Warranty Information:\n'
+            instance.descriptoin += b';'.join(validated_data['warranty'].readlines()).decode()
+        return super().update(instance, validated_data)
+
+    def create(self, validated_data):
+        validated_data.pop('warranty')
+        return Product.objects.create(**validated_data)
 
 
 class ProductStatSerializer(serializers.Serializer):
@@ -46,3 +60,7 @@ class ProductStatSerializer(serializers.Serializer):
             child=serializers.IntegerField(),
         )
     )
+
+
+
+
